0b6dcd50059baf648fd74ef2dac1111a00873fa5
Alejandra <alejandraalvarezvillarreal2002@gmail.com>
2024-03-18 21:56:14 -0500

    Add project files.

---
+++ Juego_Gato/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        GroupBox1 = New GroupBox()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        Button1 = New Button()
+        C11 = New PictureBox()
+        C21 = New PictureBox()
+        C31 = New PictureBox()
+        C12 = New PictureBox()
+        C22 = New PictureBox()
+        C32 = New PictureBox()
+        C13 = New PictureBox()
+        C23 = New PictureBox()
+        C33 = New PictureBox()
+        GroupBox1.SuspendLayout()
+        CType(C11, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C21, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C31, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C12, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C22, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C32, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C13, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C23, ComponentModel.ISupportInitialize).BeginInit()
+        CType(C33, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(78, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(297, 38)
+        Label1.TabIndex = 0
+        Label1.Text = "JUEGO DE 3 EN RAYA"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox1.Location = New Point(53, 59)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(148, 109)
+        GroupBox1.TabIndex = 1
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Inicia Juego"
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(25, 67)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(78, 24)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Circulo"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(25, 37)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(61, 24)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Cruz"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(223, 80)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(149, 48)
+        Button1.TabIndex = 2
+        Button1.Text = "Reinciar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' C11
+        ' 
+        C11.BackColor = Color.Transparent
+        C11.Location = New Point(29, 189)
+        C11.Name = "C11"
+        C11.Size = New Size(125, 62)
+        C11.SizeMode = PictureBoxSizeMode.StretchImage
+        C11.TabIndex = 3
+        C11.TabStop = False
+        ' 
+        ' C21
+        ' 
+        C21.Location = New Point(29, 257)
+        C21.Name = "C21"
+        C21.Size = New Size(125, 62)
+        C21.SizeMode = PictureBoxSizeMode.StretchImage
+        C21.TabIndex = 4
+        C21.TabStop = False
+        ' 
+        ' C31
+        ' 
+        C31.Location = New Point(29, 325)
+        C31.Name = "C31"
+        C31.Size = New Size(125, 62)
+        C31.SizeMode = PictureBoxSizeMode.StretchImage
+        C31.TabIndex = 5
+        C31.TabStop = False
+        ' 
+        ' C12
+        ' 
+        C12.Location = New Point(161, 189)
+        C12.Name = "C12"
+        C12.Size = New Size(125, 62)
+        C12.SizeMode = PictureBoxSizeMode.StretchImage
+        C12.TabIndex = 6
+        C12.TabStop = False
+        ' 
+        ' C22
+        ' 
+        C22.Location = New Point(161, 257)
+        C22.Name = "C22"
+        C22.Size = New Size(125, 62)
+        C22.SizeMode = PictureBoxSizeMode.StretchImage
+        C22.TabIndex = 7
+        C22.TabStop = False
+        ' 
+        ' C32
+        ' 
+        C32.Location = New Point(161, 325)
+        C32.Name = "C32"
+        C32.Size = New Size(125, 62)
+        C32.SizeMode = PictureBoxSizeMode.StretchImage
+        C32.TabIndex = 8
+        C32.TabStop = False
+        ' 
+        ' C13
+        ' 
+        C13.Location = New Point(292, 189)
+        C13.Name = "C13"
+        C13.Size = New Size(125, 62)
+        C13.SizeMode = PictureBoxSizeMode.StretchImage
+        C13.TabIndex = 9
+        C13.TabStop = False
+        ' 
+        ' C23
+        ' 
+        C23.Location = New Point(292, 257)
+        C23.Name = "C23"
+        C23.Size = New Size(125, 62)
+        C23.SizeMode = PictureBoxSizeMode.StretchImage
+        C23.TabIndex = 10
+        C23.TabStop = False
+        ' 
+        ' C33
+        ' 
+        C33.Location = New Point(292, 325)
+        C33.Name = "C33"
+        C33.Size = New Size(125, 62)
+        C33.SizeMode = PictureBoxSizeMode.StretchImage
+        C33.TabIndex = 11
+        C33.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LightSteelBlue
+        ClientSize = New Size(451, 410)
+        Controls.Add(C33)
+        Controls.Add(C23)
+        Controls.Add(C13)
+        Controls.Add(C32)
+        Controls.Add(C22)
+        Controls.Add(C12)
+        Controls.Add(C31)
+        Controls.Add(C21)
+        Controls.Add(C11)
+        Controls.Add(Button1)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "JUEGO DE 3 EN RAYA"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        CType(C11, ComponentModel.ISupportInitialize).EndInit()
+        CType(C21, ComponentModel.ISupportInitialize).EndInit()
+        CType(C31, ComponentModel.ISupportInitialize).EndInit()
+        CType(C12, ComponentModel.ISupportInitialize).EndInit()
+        CType(C22, ComponentModel.ISupportInitialize).EndInit()
+        CType(C32, ComponentModel.ISupportInitialize).EndInit()
+        CType(C13, ComponentModel.ISupportInitialize).EndInit()
+        CType(C23, ComponentModel.ISupportInitialize).EndInit()
+        CType(C33, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents C11 As PictureBox
+    Friend WithEvents C21 As PictureBox
+    Friend WithEvents C31 As PictureBox
+    Friend WithEvents C12 As PictureBox
+    Friend WithEvents C22 As PictureBox
+    Friend WithEvents C32 As PictureBox
+    Friend WithEvents C13 As PictureBox
+    Friend WithEvents C23 As PictureBox
+    Friend WithEvents C33 As PictureBox
+
+End Class
---
+++ Juego_Gato/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+    Dim nj As Integer
+    Dim c As Integer
+    Sub REINICIAR()
+        Dim OBJ As Control
+        For Each OBJ In Me.Controls
+            If TypeOf OBJ Is PictureBox Then
+                Dim IMG As PictureBox
+                OBJ.Tag = ""
+                IMG = OBJ
+                IMG.Image = Nothing
+            End If
+            'MessageBox.Show(OBJ.Name)
+            nj = 0
+            c = 0
+            RadioButton1.Enabled = True
+            RadioButton1.Checked = True
+            RadioButton2.Checked = False
+            RadioButton2.Enabled = True
+        Next
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        nj = 1
+        c = 0
+    End Sub
+
+    Private Sub C11_Click(sender As System.Object, ByVal e As System.EventArgs) Handles C11.Click, C12.Click, C13.Click, C21.Click, C22.Click, C23.Click, C31.Click, C32.Click, C33.Click
+        If nj <= 1 Then
+            RadioButton1.Enabled = False
+            RadioButton2.Enabled = False
+        End If
+        If sender.tag = "" Then
+            If nj Mod 2 = 0 Then
+                sender.image = Image.FromFile("C:\Users\LolaA\source\repos\Juego_Gato\Juego_Gato\Image\x.png")
+                sender.tag = "x"
+            Else
+                sender.image = Image.FromFile("C:\Users\LolaA\source\repos\Juego_Gato\Juego_Gato\Image\o.png")
+                sender.tag = "o"
+            End If
+            nj += 1
+            c += 1
+        Else
+            MessageBox.Show("Celda Ocupada por:" + sender.tag)
+        End If
+
+        If C11.Tag = sender.Tag And C12.Tag = sender.Tag And C13.Tag = sender.Tag Then
+            MsgBox("BIEN HECHO, GANO " & sender.Tag, MsgBoxStyle.Information, "ACIERTO")
+            REINICIAR()
+        ElseIf C11.Tag = sender.Tag And C21.Tag = sender.Tag And C31.Tag = sender.Tag Then
+            MsgBox("BIEN HECHO, GANO " & sender.Tag, MsgBoxStyle.Information, "ACIERTO")
+            REINICIAR()
+        ElseIf C13.Tag = sender.Tag And C23.Tag = sender.Tag And C33.Tag = sender.Tag Then
+            MsgBox("BIEN HECHO, GANO " & sender.Tag, MsgBoxStyle.Information, "ACIERTO")
+            REINICIAR()
+        ElseIf C31.Tag = sender.Tag And C32.Tag = sender.Tag And C33.Tag = sender.Tag Then
+            MsgBox("BIEN HECHO, GANO " & sender.Tag, MsgBoxStyle.Information, "ACIERTO")
+            REINICIAR()
+        ElseIf C11.Tag = sender.Tag And C22.Tag = sender.Tag And C33.Tag = sender.Tag Then
+            MsgBox("BIEN HECHO, GANO " & sender.Tag, MsgBoxStyle.Information, "ACIERTO")
+            REINICIAR()
+        ElseIf C13.Tag = sender.Tag And C22.Tag = sender.Tag And C31.Tag = sender.Tag Then
+            MsgBox("BIEN HECHO, GANO " & sender.Tag, MsgBoxStyle.Information, "ACIERTO")
+            REINICIAR()
+        ElseIf C21.Tag = sender.Tag And C22.Tag = sender.Tag And C23.Tag = sender.Tag Then
+            MsgBox("BIEN HECHO, GANO " & sender.Tag, MsgBoxStyle.Information, "ACIERTO")
+            REINICIAR()
+        ElseIf C12.Tag = sender.Tag And C22.Tag = sender.Tag And C32.Tag = sender.Tag Then
+            MsgBox("BIEN HECHO, GANO " & sender.Tag, MsgBoxStyle.Information, "ACIERTO")
+            REINICIAR()
+        ElseIf c = 9 Then
+            MsgBox("EL JUEGO TERMINO, NO HAY GANADOR.", MsgBoxStyle.Critical, "DESACIERTO")
+            REINICIAR()
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        nj = 0
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        nj = 1
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        REINICIAR()
+    End Sub
+End Class
